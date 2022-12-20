880cb3ed3d59e00cb9e3c37b8ecb77185aee6096
Shahzaib Memon <57555093+Shahzaib-D-Memon@users.noreply.github.com>
2022-12-20 11:36:56 +0500

    Update button.py

--- button.py
+++ button.py
@@ -1,7 +1,7 @@
 class Button():
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
 		self.image = image
-		self.x_pos = pos[1]
+		self.x_pos = pos[0]
 		self.y_pos = pos[1]
 		self.font = font
 		self.base_color, self.hovering_color = base_color, hovering_color
