88e85c0766c988b09faebf3cece805cf926eb44a
Shahzaib Memon <57555093+Shahzaib-D-Memon@users.noreply.github.com>
2022-12-29 22:46:03 +0500

    Update button.py

--- button.py
+++ button.py
@@ -23,7 +23,7 @@ class Button():
 		return False
 
 	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+		if position[1] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			self.text = self.font.render(self.text_input, True, self.hovering_color)
 		else:
 			self.text = self.font.render(self.text_input, True, self.base_color)
